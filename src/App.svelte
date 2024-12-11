bc891f6fb51420f9df7659a48750f510270a3623
Gabriel Maia <gabrielmaia7g@gmail.com>
2024-12-11 18:59:56 -0300

    commit inicial

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Descripion from "./components/Descripion.svelte";
+</script>
+
+<main>
+  <Descripion/>
+  <hr>
+</main>
+
+<style>
+</style>
---
+++ src/components/Descripion.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+
+</script>
+
+<div class="descripition">
+    <h1>Image hosting service</h1>
+    <h1 class="detalhe">At your home, for free.</h1>
+
+    <p>Lightweight, fast, open source and multiplatform self-hosted image backup platform</p>
+
+    <div class="ancoras">
+        <a href="">Quickstart</a>
+        <a href="">Learn More</a>
+    </div>
+</div>
+
+<style>
+
+    div{
+        font-weight: bold;
+    }
+
+    .descripition{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    h1{
+        font-size: 2em;
+    }
+
+    p{
+        margin: 20px 20px;
+        font-size: 1.5em;
+    }
+
+    .detalhe{
+        /*color: var(--cor-principal);*/
+        text-shadow: 1px 0px 10px;
+    }
+
+    .ancoras{
+        display: flex;
+        justify-content: space-around;
+        gap: 50px;
+    }
+
+    .ancoras a{
+        padding: 20px 30px;
+        border-radius: 15px;
+        background: var(--fundo2);
+        box-shadow: 0px 0px 20px #000;
+    }
+</style>
